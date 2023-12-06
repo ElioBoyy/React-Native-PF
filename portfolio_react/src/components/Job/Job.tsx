3c5fadfc7bc373a88ab6fae09d3714d45012d740
ElioBoyy <mathis.sportiello@gmail.com>
2023-12-06 10:27:49 +0100

    a

--- portfolio_react/src/components/Job/Job.tsx
+++ portfolio_react/src/components/Job/Job.tsx
@@ -1,69 +1,114 @@
 import { useState } from "react";
-import './Job.css';
-import { urlRegex } from '../../data/techno';
+import "./Job.css";
+import { urlRegex } from "../../data/techno";
 
 interface JobProps {
-    job: any;
+  job: any;
 }
 
 function Job({ job }: JobProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
 
-    const [isExpanded, setIsExpanded] = useState(false);
+  const [buttonText, setButtonText] = useState("+");
+  const handleClick = () => {
+    invIsExpanded();
+    setButtonText((prevText) => (prevText === "+" ? "-" : "+"));
+  };
 
-    const [buttonText, setButtonText] = useState("+");
-    const handleClick = () => {
-        invIsExpanded();
-        setButtonText((prevText) => (prevText === "+" ? "-" : "+"));
-    };
+  const invIsExpanded = () => {
+    setIsExpanded((prev) => !prev);
+  };
 
-    const invIsExpanded = () => {
-        setIsExpanded((prev) => !prev)
-    }
+  const regexDrive: RegExp = /^(?!https?:\/\/drive.google.com\/).+/;
 
-    return (
-        <article className="job-article">
-            <div className="compagny">
-                <h3>{job[0]}</h3>
-                <a href={job[2]} target="_blank">
-                    <img
-                        src={job[1]}
-                        alt=""
-                    />
-                </a>
+  return (
+    <article className="job-article">
+      <div className="compagny">
+        <h2>{job[0]}</h2>
+        <a href={job[2]} target="_blank">
+          <img src={job[1]} alt="" />
+        </a>
+      </div>
+      <div className="job">
+        <h2 className="libelle-job">{job[3]}</h2>
+        <p dangerouslySetInnerHTML={{ __html: job[4] }}></p>
+      </div>
+      <div className={`content ${isExpanded ? "active" : ""}`}>
+        {job[5].length > 0 && (
+          <>
+            <h2>Hard skills</h2>
+            <div className="flex-skills">
+              {job[5].map((techno: string) =>
+                urlRegex.test(techno) ? (
+                  <p
+                    className="p_badges"
+                    dangerouslySetInnerHTML={{ __html: techno }}
+                  ></p>
+                ) : (
+                  <img
+                    src={techno}
+                    alt=""
+                    className="github_badges"
+                    key={techno}
+                  />
+                )
+              )}
             </div>
-            <div className="job">
-                <p className="libelle-job">{job[3]}</p>
-                <p dangerouslySetInnerHTML={{ __html: job[4] }}></p>
-            </div>
-            <div className={`content ${isExpanded ? 'active' : ''}`}>
-                <h2>Hard skills</h2>
-                <div className="flex-skills">
-                    {
-                        job[5].map((techno: string) => (
-                            urlRegex.test(techno) ? (
-                                <p className="p_badges" dangerouslySetInnerHTML={{ __html: techno }}></p>
-                            ) : (
-                                <img src={techno} alt="" className="github_badges" key={techno} />
-                            )
-                        ))
-                    }
-                </div>
+          </>
+        )}
 
-                <h2>Soft skills</h2>
-                <div className="flex-skills">
-                    {
-                        job[6].map((techno: string) => (
-                            urlRegex.test(techno) ? (
-                                <p className="p_badges" dangerouslySetInnerHTML={{ __html: techno }}></p>
-                            ) : (
-                                <img src={techno} alt="" className="github_badges" key={techno} />
-                            )
-                        ))
-                    }
-                </div>
+        {job[6].length > 0 && (
+          <>
+            <h2>Soft skills</h2>
+            <div className="flex-skills">
+              {job[6].map((techno: string) =>
+                urlRegex.test(techno) ? (
+                  <p
+                    className="p_badges"
+                    dangerouslySetInnerHTML={{ __html: techno }}
+                  ></p>
+                ) : (
+                  <img
+                    src={techno}
+                    alt=""
+                    className="github_badges"
+                    key={techno}
+                  />
+                )
+              )}
             </div>
-            <button className="open_more_infos" onClick={handleClick}>{buttonText}</button>
-        </article>)
+          </>
+        )}
+
+        {job[7] && job[7].length > 0 && (
+          <>
+            <h2>Pour compléter</h2>
+            <div className="flex-skills">
+              {job[7].map((techno: string) =>
+                urlRegex.test(techno) ? (
+                  <p
+                    className="p_badges"
+                    dangerouslySetInnerHTML={{ __html: techno }}
+                  ></p>
+                ) : (
+                  <a href={techno} target="_blank" key={techno}>
+                    {regexDrive.test(techno) ? (
+                      <p>Visitez le repo GitHub</p>
+                    ) : (
+                      <p>Télécharger ma lettre de recommandation</p>
+                    )}
+                  </a>
+                )
+              )}
+            </div>
+          </>
+        )}
+      </div>
+      <button className="open_more_infos" onClick={handleClick}>
+        {buttonText}
+      </button>
+    </article>
+  );
 }
 
 export default Job;
