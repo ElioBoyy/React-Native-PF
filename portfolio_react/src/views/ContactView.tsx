6c1971725d5fe6e51877feca2a593f4bb2f7c928
Mathis SPORTIELLO <sportiem@iut-acy.local>
2023-12-15 16:35:35 +0100

    Some changes

--- portfolio_react/src/views/ContactView.tsx
+++ portfolio_react/src/views/ContactView.tsx
@@ -1,53 +1,82 @@
-import { useEffect } from "react";
+import React, { useState } from "react";
 import "./ContactView.css";
-import LinkedinLogo from "../assets/iconmonstr-linkedin-3.svg"
-import GithubLogo from "../assets/iconmonstr-github-3.svg"
-import InstaLogo from "../assets/iconmonstr-instagram-11.svg"
+import LinkedinLogo from "../assets/iconmonstr-linkedin-3.svg";
+import GithubLogo from "../assets/iconmonstr-github-3.svg";
+import InstaLogo from "../assets/iconmonstr-instagram-11.svg";
 
 function ContactView({ langue }: { langue: string }) {
+  const [clickedSocial, setClickedSocial] = useState("");
 
-    useEffect(() => {
-        var Social = document.getElementById('social-network');
-        var SocialText = document.getElementById('social-network-txtzone');
-        var Logos = document.getElementsByClassName('social-network-logos');
-        var SocialTextWidth = 0;
+  const handleSocialClick = (state: string) => {
+    setClickedSocial(state);
+  };
 
-        if (SocialText) {
-            SocialTextWidth = SocialText.offsetWidth;
-        } else {
-            console.error('Element not found');
-        }
-
-        if (Logos.length > 0) {
-            const socialWidth = window.innerWidth;
-            for(var i = 1; i <= Logos.length ; i++){
-                const firstLogo = Logos[0] as HTMLElement;
-                firstLogo.style.width = (socialWidth * .8 - 82 - SocialTextWidth).toString();
-            }
-          } else {
-            console.error('Logo element not found');
-          }
-    }, []);
-
-    
-
-    return (
-        <div id="contact-view">
-            <section id="social-network">
-                <div id="social-network-txtzone">
-                    <h1>Linkedin</h1>
-                    <h1>GitHub</h1>
-                    <h1>Instagram</h1>
-                </div>
-                <div style={{ flex:0, position:"relative", height:"100%", width:"0px", border:"1px solid black", marginLeft:"80px", marginRight:"80px"}} />
-                <div className="social-network-logos">
-                    <img src={LinkedinLogo} alt="" />
-                    <img src={GithubLogo} alt="" />
-                    <img src={InstaLogo} alt="" />
-                </div>
-            </section>
+  return (
+    <div id="contact-view">
+      <section id="social-network">
+        <div id="social-network-txtzone">
+          <h1
+            id="linkedin"
+            onClick={() => handleSocialClick("linkedin")}
+            className={clickedSocial === "linkedin" ? "active" : ""}
+          >
+            Linkedin
+          </h1>
+          <h1
+            id="github"
+            onClick={() => handleSocialClick("github")}
+            className={clickedSocial === "github" ? "active" : ""}
+          >
+            GitHub
+          </h1>
+          <h1
+            id="instagram"
+            onClick={() => handleSocialClick("instagram")}
+            className={clickedSocial === "instagram" ? "active" : ""}
+          >
+            Instagram
+          </h1>
         </div>
-    );
+        <div
+          style={{
+            flex: 0,
+            position: "relative",
+            height: "100%",
+            width: "0px",
+            border: "1px solid black",
+            marginLeft: "80px",
+            marginRight: "80px"
+          }}
+        />
+        <div className="social-network-logos">
+          <img
+            src={LinkedinLogo}
+            alt=""
+            style={{
+              transform:
+                clickedSocial === "linkedin" ? "translateY(0px)" : "translateY(-300px)"
+            }}
+          />
+          <img
+            src={GithubLogo}
+            alt=""
+            style={{
+              transform:
+                clickedSocial === "github" ? "translateY(-300px)" : clickedSocial === "instagram" ? "translateY(-600px)" : "translateY(0px)"
+            }}
+          />
+          <img
+            src={InstaLogo}
+            alt=""
+            style={{
+              transform:
+                clickedSocial === "instagram" ? "translateY(-600px)" : "translateY(-300px)"
+            }}
+          />
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default ContactView;
