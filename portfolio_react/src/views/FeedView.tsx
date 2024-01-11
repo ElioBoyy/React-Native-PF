982a124b44f1800a844969e40527525043812fad
ElioBoyy <75843356+ElioBoyy@users.noreply.github.com>
2024-01-11 17:32:45 +0100

    Update FeedView.tsx
    Mobile edit

--- portfolio_react/src/views/FeedView.tsx
+++ portfolio_react/src/views/FeedView.tsx
@@ -4,6 +4,7 @@ import { data } from "../data/data";
 function FeedView({ langue }: { langue: string }) {
   return <>
     <div id="FeedViewCSS">
+      {langue === 'fr' ? (
       <article id="gym" style={{flexDirection: "row-reverse"}}>
         <img src={data.fr["feed"][0][0]} style={{margin: '-100px'}}></img>
         <div className="FeedViewTxt">
@@ -24,7 +25,21 @@ function FeedView({ langue }: { langue: string }) {
           <h1 dangerouslySetInnerHTML={{ __html: data.fr["feed"][2][1] }}></h1>
           <p dangerouslySetInnerHTML={{ __html: data.fr["feed"][2][2] }}></p>
         </div>
+      </article> ) : (
+          <article id="gym" style={{flexDirection: "row-reverse"}}>
+        <img src={data.en["feed"][0][0]} style={{margin: '-100px'}}></img>
+        <div className="FeedViewTxt">
+          <h1 dangerouslySetInnerHTML={{ __html: data.en["feed"][0][1] }}></h1>
+          <p dangerouslySetInnerHTML={{ __html: data.en["feed"][0][2] }}></p>
+        </div>
       </article>
+      <article id="plane">
+        <video src={data.en["feed"][1][0]} autoPlay muted loop></video>
+        <div className="FeedViewTxt">
+          <h1 dangerouslySetInnerHTML={{ __html: data.en["feed"][1][1] }}></h1>
+          <p dangerouslySetInnerHTML={{ __html: data.en["feed"][1][2] }}></p>
+        </div>
+      </article> )}
     </div>
   </>
 }
